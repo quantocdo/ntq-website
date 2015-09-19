f56a5b7908a5ecef14d33692b5140971a601beb6
LongLH <d@dapps.me>
2015-09-19 18:53:15 +0700

    fixed some links for demo version

--- app/server/config/profile/nighty.js
+++ app/server/config/profile/nighty.js
@@ -3,13 +3,30 @@
 exports._ = '/config/profile/nighty';
 exports._factory = function() {
 	return {
-		host: 'nighty.ntq-solution.com.vn',
+		domain: {
+			protocol: 'http://',
+			base: 'ntq-solution.com.vn',
+			locales: {
+				en: 'www.ntq-solution.com.vn',
+				ja: 'ja.ntq-solution.com.vn',
+			},
+		},
 		port: 4000,
-		db: 'mongodb://localhost/ntq-website-nighty',
+		db: 'mongodb://localhost/ntq-website',
 		assets: {
-			cdn: '//ntq.local/o',
-			default: false,
+			cdn: '//static.ntq-solution.com.vn',
+			default: true,
 			purgeCache: false,
 		},
+		upload: {
+			doc: '../../../ntq-website-v1/upload/doc'
+		},
+		mail: {
+			to: 'dzung.nguyen@ntq-solution.com.vn',
+			from: 'no-reply@they.online',
+			region: 'us-west-2',
+			accessKeyId: 'AKIAI6W6J2H4K4W3OZKA',
+			secretAccessKey: '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'
+		}
 	};
 };
