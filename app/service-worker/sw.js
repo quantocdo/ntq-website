523b89e0417b3604a54d35ecd94d3315a8a9d5a5
LongLH <d@dapps.me>
2017-08-22 11:22:32 +0700

    update sw

--- app/service-worker/sw.js
+++ app/service-worker/sw.js
@@ -1,13 +1,39 @@
-var CACHE_NAME = 'ntq-solution-cache-v2';
+var VERSION = 'v1';
+var CACHE_NAME = 'ntq-solution-cache';
 var ASSET_PREFIXES = [
+	'https://fonts.gstatic.com',
+	'https://fonts.googleapis.com',
+	'https://www.ntq-solution.com.vn/img',
+	'https://www.ntq-solution.com.vn/css',
+	'https://www.ntq-solution.com.vn/js',
+	'https://jp.ntq-solution.com.vn/img',
+	'https://jp.ntq-solution.com.vn/css',
+	'https://jp.ntq-solution.com.vn/js',
+	'https://example.com/img',
+	'https://example.com/css',
+	'https://example.com/js',
+	'https://example.com/bower_components',
+	'https://example.com/node_modules'
+];
+var ORIGINS = [
 	'https://www.ntq-solution.com.vn',
 	'https://jp.ntq-solution.com.vn',
-	'https://example.com',
-	'https://fonts.gstatic.com/s/raleway/v11/0dTEPzkLWceF7z0koJaX1A.woff2',
-	'https://fonts.googleapis.com/css?family=Raleway'
+	'https://example.com'
 ];
 
 self.addEventListener('install', function(event) {
+	console.debug('clear cache...');
+
+	event.waitUntil(
+		caches.keys().then(function(keyList) {
+			return Promise.all(
+				keyList.map(function(key) {
+					console.debug('deleting key...', key);
+					return caches.delete(key);
+				})
+			);
+		})
+	);
 });
 
 self.addEventListener('fetch', function(event) {
@@ -16,7 +42,13 @@ self.addEventListener('fetch', function(event) {
 	// filter request
 	var skip = ASSET_PREFIXES.every(function(prefix) {
 		return url.indexOf(prefix) !== 0;
-	})
+	});
+
+	var isSameOrigin = ORIGINS.some(function(origin) {
+		return url.indexOf(origin) === 0;
+	});
+
+	console.debug('fetching... ', event.request.url, '... looking from cache', !skip, isSameOrigin);
 
 	if (skip) {
 		return;
@@ -26,14 +58,20 @@ self.addEventListener('fetch', function(event) {
 		caches.open(CACHE_NAME).then(function(cache) {
 			return cache.match(event.request.url).then(function(response) {
 				if (response) {
+					console.debug('from cache: ', event.request.url, response.status);
+
 					return response;
 				}
 
 				var cloneRequest = event.request.clone();
 
 				return fetch(cloneRequest).then(function(response) {
-					// add to cache
-					cache.put(url, response.clone());
+					console.debug('from network', event.request.url, response.status);
+
+					if (response.type === 'opaque' || response.ok) {
+						// add to cache
+						cache.put(url, response.clone());
+					}
 
 					return response;
 				});
